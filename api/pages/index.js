c6b4e780c9575cfd33751de9d9e166ad1629350f
reonfx <kartiksinghparmar9893@gmail.com>
2025-11-14 21:18:20 +0530

    Delete api directory

--- api/pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{fontFamily: 'system-ui', padding: 20}}>
-      <h1>Nominal API Working</h1>
-      <p>Your API is deployed successfully.</p>
-    </div>
-  );
-}
