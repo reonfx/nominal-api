fd5040fc40fa840b364796da9cf6add509883110
reonfx <kartiksinghparmar9893@gmail.com>
2025-11-14 20:58:53 +0530

    Create index.js

---
+++ api/pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{fontFamily: 'system-ui, Arial', padding: 24}}>
+      <h1>Nominal API Active</h1>
+      <p>Your API is deployed successfully 🎉</p>
+    </div>
+  )
+  }
